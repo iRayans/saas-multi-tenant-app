5d8d205d1e853a1039404d2f4dda7e7edfc70b66
rharbi <iirharbi@gmail.com>
2026-05-20 19:47:32 +0300

    first commit

---
+++ src/main/java/com/rayan/saasapp/SaasMultiTenantAppApplication.java
@@ -0,0 +1,13 @@
+package com.rayan.saasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasMultiTenantAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaasMultiTenantAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rayan/saasapp/SaasMultiTenantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rayan.saasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasMultiTenantAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
