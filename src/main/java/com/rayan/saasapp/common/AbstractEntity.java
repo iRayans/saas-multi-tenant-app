9e57ee37e37da75aa6c5502920366182a3155680
rharbi <iirharbi@gmail.com>
2026-05-23 01:12:13 +0300

    Create docker-compose.yaml file

---
+++ src/main/java/com/rayan/saasapp/common/AbstractEntity.java
@@ -0,0 +1,50 @@
+package com.rayan.saasapp.common;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.UUID;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private String id;
+    @Column(name = "tenant_id", nullable = false)
+    private String tenantId;
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+    @Column(name = "updated_at", insertable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+    @Column(name = "deleted", nullable = false)
+    private Boolean deleted;
+
+    @PrePersist
+    public void onCreate() {
+        if (this.deleted == null) {
+            this.deleted = false;
+        }
+        if (this.tenantId == null) {
+            this.tenantId = "default";
+        }
+    }
+}
