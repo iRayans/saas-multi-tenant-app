407cc7b8cda435fe12c27a0a454ff24e8591a6b5
rharbi <iirharbi@gmail.com>
2026-06-04 20:47:23 +0300

    Add Schema support to TenantContext.java

--- src/main/java/com/rayan/saasapp/config/TenantContext.java
+++ src/main/java/com/rayan/saasapp/config/TenantContext.java
@@ -2,8 +2,10 @@ package com.rayan.saasapp.config;
 
 public class TenantContext {
     private static final ThreadLocal<String> CURRENT_TENANT = new ThreadLocal<>();
+    private static final ThreadLocal<String> CURRENT_SCHEMA = new ThreadLocal<>();
 
 
+    // TENANT
     public static String getCurrentTenant() {
         return CURRENT_TENANT.get();
     }
@@ -14,5 +16,15 @@ public class TenantContext {
 
     public static void clearCurrentTenant() {
         CURRENT_TENANT.remove();
+        CURRENT_SCHEMA.remove();
+    }
+
+    // SCHEMA
+    public static String getCurrentSchema() {
+        return CURRENT_SCHEMA.get();
+    }
+
+    public static void setCurrentSchema(String tenant) {
+        CURRENT_SCHEMA.set(tenant);
     }
 }
