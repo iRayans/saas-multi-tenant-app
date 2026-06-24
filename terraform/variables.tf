fae3dcf86cfbab3f04a2ff415361b3106ebafda3
rharbi <iirharbi@gmail.com>
2026-06-24 06:12:48 +0300

    initiate terraform script

---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "app_name" {
+  description = "Application name used as prefix for resource names"
+  type        = string
+  default     = "saas-app"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "db_username" {
+  description = "RDS master username"
+  type        = string
+  default     = "postgres"
+}
+
+variable "db_password" {
+  description = "RDS master password"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_name" {
+  description = "Initial database name"
+  type        = string
+  default     = "saasdb"
+}
