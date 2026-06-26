c152aa46d7573a2ced4af8fc910f93dff8040c70
rharbi <iirharbi@gmail.com>
2026-06-26 05:12:31 +0300

    Auto build and push image to the ECR

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -10,3 +10,19 @@ resource "aws_ecr_repository" "app" {
     Name = var.app_name
   }
 }
+
+# After ECR resource get created - build the project and push it to ECR.
+resource "null_resource" "docker_push" {
+  triggers = {
+    image_tag = "latest"
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${aws_ecr_repository.app.repository_url}
+      docker buildx build --platform linux/amd64 -t ${aws_ecr_repository.app.repository_url}:latest --push ../
+    EOT
+  }
+
+  depends_on = [aws_ecr_repository.app]
+}
